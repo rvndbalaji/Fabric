19acdfe01488cbc262e5f90915bcfce22fca1ade
Aravind Balaji <aravind.balaji@live.com>
2017-10-10 16:24:21 +0530

    First upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<!--Created by Aravind on 10 Oct 2017-->
+<html lang="en">
+
+<head>
+    <!--jQUERY should always come first before Bootstrap-->
+    <base target="_blank">
+    <script src="frameworks/jquery-3.2.1.js"></script>
+    <link rel="icon" href="images/dp.png">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Fabric</title>
+
+    <!----------------BOOTSTRAP----------------------->
+
+    <link rel="stylesheet" href="frameworks/bootstrap.min.css">
+    <!----------------GOOGLE FONTS----------------------->
+    <link href="https://fonts.googleapis.com/css?family=Spectral" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="frameworks/animate.min.css">
+
+    <!--SMOOTH SCROLL-->
+    <script src="frameworks/TweenMax.min.js"></script>
+    <script src="frameworks/ScrollToPlugin.min.js"></script>
+
+    <!-----SLICK>
+
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
+<!-- Add the slick-theme.css if you want default styling -->
+    <!--link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick-theme.css" /-->
+    <!---------------------------------------------------------------------------------------------------->
+</head>
+
+
+
+
+<body>
+    <nav class="navbar navbar-fixed-top menu">
+        <div class="container">
+            Fabric
+        </div>
+    </nav>
+</body>
