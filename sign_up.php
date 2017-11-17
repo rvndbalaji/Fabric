26e986bdd5b776b567f7d4d64c3ed120dc637352
Aravind Balaji <aravind.balaji@live.com>
2017-11-17 16:35:00 +0530

    still designing

--- sign_up.php
+++ sign_up.php
@@ -41,13 +41,13 @@ require_once('db_core.php');
 
                 <div class="row">
                     <div class="col-12">
-                        <span class="custom_label">First Name</span><input class="input_box" type="text" id="firstname_text" />
+                        <span class="custom_label">First Name</span><input class="input_box" type="text" name="firstname_text" />
                     </div>
                 </div><br>
 
                 <div class="row">
                     <div class="col-12">
-                        <span class="custom_label">Last Name</span><input class="input_box" type="text" id="lastname_text" />
+                        <span class="custom_label">Last Name</span><input class="input_box" type="text" name="lastname_text" />
                     </div>
                 </div><br>
 
@@ -55,18 +55,29 @@ require_once('db_core.php');
 
                 <div class="row">
                     <div class="col-12">
-                        <span class="custom_label">Username</span><input class="input_box" type="text" id="username_text" />
+                        <span class="custom_label">Username</span><input class="input_box" type="text" name="username_text" />
                     </div>
                 </div><br>
                 <div class="row">
                     <div class="col-12">
-                        <span class="custom_label">Password</span><input class="input_box" id="password_text" type="password" />
+                        <span class="custom_label">Password</span><input class="input_box" name="password_text" type="password" />
                         </label>
                     </div>
                 </div><br>
+
                 <div class="row">
                     <div class="col-12">
-                        <span class="custom_button" id="login_button" onmouseup="performSignUp();">Sign Up</span>
+                        <span class="custom_label">Email</span><input class="input_box" name="email_text" type="text" />
+                        </label>
+                    </div>
+                </div><br>
+
+
+
+
+                <div class="row">
+                    <div class="col-12">
+                        <span class="custom_button" name="login_button" onmouseup="performSignUp();">Sign Up</span>
                     </div>
                 </div><br><br>
                 <div class="row">
